a1a89f7e801cb1f121bcc57993d0719481cf68a4
Valerian Roustit <valerian.roustit@gmail.com>
2020-07-07 16:17:46 +0100

    add abilities

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -4,11 +4,15 @@ const PokemonDetail = ({pokemon}) => {
 
     if(!pokemon) return null;
 
+    const abilitiesName = [];
+    pokemon.abilities.forEach( element => abilitiesName.push(element.ability.name));
+
     return (
         <div id ="pokemon-detail">
             <h3>Pokemon name: {pokemon.name.toUpperCase()}</h3>
             <h3>Weight: {pokemon.weight} kg</h3>
             <h3>Height: {pokemon.height * 10} cm</h3>
+            <h3>Abilities: {abilitiesName}</h3>
         </div>
     )
 }
